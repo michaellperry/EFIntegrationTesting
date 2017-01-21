00469bf5205c3ef2a64b12c3aacf3d78993982d1
Michael L Perry <michael@qedcode.com>
2017-01-21 16:55:13 -0600

    Added EF Core.

---
+++ EFCore/Globalmantics.DAL/Entities/CartLine.cs
@@ -0,0 +1,14 @@
+﻿namespace Globalmantics.DAL.Entities
+{
+    public class CartLine
+    {
+        public int CartLineId { get; set; }
+
+        public Cart Cart { get; set; }
+        public int CartId { get; set; }
+
+        public string Description { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Quantity { get; set; }
+    }
+}
---
+++ EFCore/Globalmantics.DAL/GlobalmanticsContext.cs
@@ -0,0 +1,24 @@
+﻿using Globalmantics.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Globalmantics.DAL
+{
+    public class GlobalmanticsContext : DbContext
+    {
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartLine> CartLines { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("GolbalmanticsContext");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CartLine>()
+                .Property(x => x.Description)
+                .HasMaxLength(50)
+                .IsRequired();
+        }
+    }
+}
