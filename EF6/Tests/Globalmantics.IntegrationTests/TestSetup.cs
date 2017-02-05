96fddbca132d756ec67e265f3d87f668605ef311
Michael L Perry <michael@qedcode.com>
2017-02-05 01:44:04 -0600

    Delete the database.

--- EF6/Tests/Globalmantics.IntegrationTests/TestSetup.cs
+++ EF6/Tests/Globalmantics.IntegrationTests/TestSetup.cs
@@ -1,14 +1,10 @@
 ﻿using Globalmantics.DAL;
+using Globalmantics.DAL.Migrations;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Globalmantics.DAL.Migrations;
-using System.Configuration;
 using System.IO;
 using System.Reflection;
 
@@ -20,29 +16,84 @@ namespace Globalmantics.IntegrationTests
         [OneTimeSetUp]
         public void InitializeDatabase()
         {
-            var master = new SqlConnectionStringBuilder
+            Destroy();
+            Create();
+        }
+
+        [OneTimeTearDown]
+        public void TearDownDatabase()
+        {
+            Destroy();
+        }
+
+        private static void Create()
+        {
+            ExecuteSqlCommand(Master, $@"
+                CREATE DATABASE [Globalmantics]
+                ON (NAME = 'Globalmantics',
+                FILENAME = '{FileName}')");
+
+            var migration = new MigrateDatabaseToLatestVersion<GlobalmanticsContext, GlobalmanticsConfiguration>();
+            migration.InitializeDatabase(new GlobalmanticsContext());
+        }
+
+        private static void Destroy()
+        {
+            var fileNames = ExecuteSqlQuery(Master, @"
+                SELECT [physical_name] FROM [sys].[master_files]
+                WHERE [database_id] = DB_ID('Globalmantics')", row =>
+                (string)row["physical_name"]);
+            ExecuteSqlCommand(Master, @"
+                EXEC sp_detach_db 'Globalmantics'");
+
+            fileNames.ForEach(File.Delete);
+        }
+
+        private static void ExecuteSqlCommand(SqlConnectionStringBuilder connectionStringBuilder, string commandText)
+        {
+            using (var connection = new SqlConnection(connectionStringBuilder.ConnectionString))
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = commandText;
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private static List<T> ExecuteSqlQuery<T>(SqlConnectionStringBuilder connectionStringBuilder, string queryText, Func<SqlDataReader, T> read)
+        {
+            var result = new List<T>();
+            using (var connection = new SqlConnection(connectionStringBuilder.ConnectionString))
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = queryText;
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            result.Add(read(reader));
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        private static SqlConnectionStringBuilder Master =>
+            new SqlConnectionStringBuilder
             {
                 DataSource = @"(LocalDB)\MSSQLLocalDB",
                 InitialCatalog = "master",
                 IntegratedSecurity = true
             };
-            var codeBase = new Uri(Assembly.GetAssembly(typeof(GlobalmanticsContext)).CodeBase);
-            var filename = Path.Combine(Path.GetDirectoryName(codeBase.AbsolutePath), "Globalmantics.mdf");
 
-            using (var connection = new SqlConnection(master.ConnectionString))
-            {
-                connection.Open();
-                using (var command = connection.CreateCommand())
-                {
-                    command.CommandText = $@"CREATE DATABASE [Globalmantics]
-                        ON (NAME = 'Globalmantics',
-                        FILENAME = '{filename}')";
-                    command.ExecuteNonQuery();
-                }
-            }
-
-            var migration = new MigrateDatabaseToLatestVersion<GlobalmanticsContext, GlobalmanticsConfiguration>();
-            migration.InitializeDatabase(new GlobalmanticsContext());
-        }
+        private static string FileName => Path.Combine(
+            Path.GetDirectoryName(
+                Assembly.GetExecutingAssembly().Location),
+            "Globalmantics.mdf");
     }
 }
