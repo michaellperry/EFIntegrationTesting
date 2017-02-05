489dd0f2346bc6ac0e6f265d7cf6bb0ea6cefbf6
Michael L Perry <michael@qedcode.com>
2017-02-05 16:44:10 -0600

    Added test setup.

---
+++ EF6/Tests/Globalmantics.IntegrationTests/TestSetup.cs
@@ -0,0 +1,55 @@
+﻿using Globalmantics.DAL;
+using Globalmantics.DAL.Migrations;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Globalmantics.IntegrationTests
+{
+    public class TestSetup
+    {
+        private static void ExecuteSqlCommand(
+            SqlConnectionStringBuilder connectionStringBuilder,
+            string commandText)
+        {
+            using (var connection = new SqlConnection(connectionStringBuilder.ConnectionString))
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = commandText;
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private static List<T> ExecuteSqlQuery<T>(
+            SqlConnectionStringBuilder connectionStringBuilder,
+            string queryText,
+            Func<SqlDataReader, T> read)
+        {
+            var result = new List<T>();
+            using (var connection = new SqlConnection(connectionStringBuilder.ConnectionString))
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = queryText;
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            result.Add(read(reader));
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
