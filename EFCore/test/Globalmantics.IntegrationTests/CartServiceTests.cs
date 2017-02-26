4b515d22d445f3f843ab53fa63d1d87389045098
Michael L Perry <michael@qedcode.com>
2017-02-26 16:15:39 -0600

    Isolate tests with transactions (EF Core).

--- EFCore/test/Globalmantics.IntegrationTests/CartServiceTests.cs
+++ EFCore/test/Globalmantics.IntegrationTests/CartServiceTests.cs
@@ -25,5 +25,26 @@ namespace Globalmantics.IntegrationTests
                 cart.CartItems.Count().Should().Be(0);
             }
         }
+
+        [Fact]
+        public void CanAddItemToCart()
+        {
+            using (var context = GivenGlobalmanticsContext())
+            {
+                var userService = new UserService(context);
+                var cartService = new CartService(context);
+
+                var user = userService.GetUserByEmail("test@globalmantics.com");
+                context.SaveChanges();
+
+                var cart = cartService.GetCartForUser(user);
+                context.SaveChanges();
+
+                cartService.AddItemToCart(cart, "CAFE-314", 2);
+                context.SaveChanges();
+
+                cart.CartItems.Count().Should().Be(1);
+            }
+        }
     }
 }
