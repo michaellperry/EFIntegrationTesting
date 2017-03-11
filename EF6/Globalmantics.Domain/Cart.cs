f85e08047d375174b273a626927ba4b330b884e7
Michael L Perry <michael@qedcode.com>
2017-03-11 14:56:59 -0600

    Group items of similar kind.

--- EF6/Globalmantics.Domain/Cart.cs
+++ EF6/Globalmantics.Domain/Cart.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Globalmantics.Domain
 {
@@ -12,20 +13,30 @@ namespace Globalmantics.Domain
 
         public int CartId { get; private set; }
 
-        public virtual User User { get; private set; }
+        public User User { get; private set; }
         public int UserId { get; private set; }
 
-        public virtual ICollection<CartItem> CartItems { get; }
+        public ICollection<CartItem> CartItems { get; }
 
         public DateTime CreatedAt { get; private set; }
 
         public void AddItem(CatalogItem catalogItem, int quantity)
         {
-            CartItems.Add(new CartItem
+            var existingItem = CartItems
+                .Where(x => x.CatalogItem == catalogItem)
+                .FirstOrDefault();
+            if (existingItem != null)
             {
-                CatalogItem = catalogItem,
-                Quantity = quantity
-            });
+                existingItem.Quantity += quantity;
+            }
+            else
+            {
+                CartItems.Add(new CartItem
+                {
+                    CatalogItem = catalogItem,
+                    Quantity = quantity
+                });
+            }
         }
 
         public static Cart Create(int userId)
--- EF6/Tests/Globalmantics.UnitTests/CartTests.cs
+++ EF6/Tests/Globalmantics.UnitTests/CartTests.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using Globalmantics.Domain;
 using NUnit.Framework;
+using System.Linq;
 
 namespace Globalmantics.UnitTests
 {
@@ -19,10 +20,24 @@ namespace Globalmantics.UnitTests
         public void CanAddItemToCart()
         {
             var cart = Cart.Create(0);
+            var catalogItem = new CatalogItem() { Sku = "CAFE-314" };
 
-            cart.AddItem(new CatalogItem() { Sku = "CAFE-314" }, 2);
+            cart.AddItem(catalogItem, 2);
 
             cart.CartItems.Count.Should().Be(1);
+        }
+
+        [Test]
+        public void GroupsItemsOfSameKind()
+        {
+            var cart = Cart.Create(0);
+            var catalogItem = new CatalogItem() { Sku = "CAFE-314" };
+
+            cart.AddItem(catalogItem, 2);
+            cart.AddItem(catalogItem, 1);
+
+            cart.CartItems.Count.Should().Be(1);
+            cart.CartItems.Single().Quantity.Should().Be(3);
         }
     }
 }
