8604ac829286f48ebe8d5687d96a84420bcd839e
Michael L Perry <michael@qedcode.com>
2017-03-12 20:26:55 -0500

    Initialize test data for unit tests.

--- EF6/Tests/Globalmantics.UnitTests/CartServiceTests.cs
+++ EF6/Tests/Globalmantics.UnitTests/CartServiceTests.cs
@@ -15,5 +15,54 @@ namespace Globalmantics.UnitTests
     [TestFixture]
     public class CartServiceTests
     {
+        [Test]
+        public void CanLoadCartWithNoItems()
+        {
+            var context = new InMemoryDataContext();
+            var repository = new Repository(context);
+            var userService = new UserService(repository);
+            var cartService = new CartService(repository);
+
+            var user = userService.GetUserByEmail("test@globalmantics.com");
+            context.Commit();
+            var cart = cartService.GetCartForUser(user);
+            context.Commit();
+
+            cart.CartItems.Count().Should().Be(0);
+        }
+
+        [Test]
+        public void CanLoadCartWithOneItem()
+        {
+            var context = new InMemoryDataContext();
+            InitializeCartWithOneItem(context);
+
+            var repository = new Repository(context);
+            var userService = new UserService(repository);
+            var cartService = new CartService(repository);
+
+            var user = userService.GetUserByEmail("test@globalmantics.com");
+            context.Commit();
+            var cart = cartService.GetCartForUser(user);
+            context.Commit();
+
+            cart.CartItems.Count().Should().Be(1);
+            cart.CartItems.Single().Quantity.Should().Be(2);
+        }
+
+        private void InitializeCartWithOneItem(InMemoryDataContext context)
+        {
+            var user = context.Add(User.Create("test@globalmantics.com"));
+            context.Commit();
+            var cart = context.Add(Cart.Create(user.UserId));
+            var catalogItem = context.Add(CatalogItem.Create
+            (
+                sku: "CAFE-314",
+                description: "1 Pound Guatemalan Coffee Beans",
+                unitPrice: 18.80m
+            ));
+            cart.AddItem(catalogItem, 2);
+            context.Commit();
+        }
     }
 }
