bec6c736aa64f7dbe67ff680340ee1b8e0f40f8c
Michael L Perry <michael@qedcode.com>
2017-04-29 16:22:00 -0500

    Removed namespaces.

--- EF6/Globalmantics.Logic/Queries/UserByEmail.cs
+++ EF6/Globalmantics.Logic/Queries/UserByEmail.cs
@@ -1,11 +1,7 @@
 ﻿using Globalmantics.Domain;
 using Highway.Data;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Globalmantics.Logic.Queries
 {
